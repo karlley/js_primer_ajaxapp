6ba373915a2815b4bc590255b7edd37c576012b2
karlley <karlley.jp@gmail.com>
2023-08-23 07:26:11 +0900

    p383 まで

--- index.js
+++ index.js
@@ -6,11 +6,43 @@ function fetchUserInfo(userId) {
         console.error("エラーレスポンス", response); //ユーザーが取得できない時
       } else {
         return response.json().then((userInfo) => {
-          console.log(userInfo);
+          const view = escapeHTML`
+          <h4>${userInfo.name} (@${userInfo.login})</h4>
+          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+          <dl>
+            <dt>Location</dt>
+            <dt>${userInfo.location}</dt>
+            <dt>Repositories</dt>
+            <dd>${userInfo.public_repos}</dd>
+          </dl>
+          `;
+
+          const result = document.getElementById("result");
+          result.innerHTML = view;
         });
       }
     })
     .catch((error) => {
       console.error(error); //通信自体が失敗した時
     });
+}
+
+function escapeSpecialCharas(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === "string") {
+      return result + escapeSpecialCharas(value) + str;
+    } else {
+      return result + String(value) + str;
+    }
+  });
 }
