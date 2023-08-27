776fbe8879a5765cb83858ba19ab92a664c34738
karlley <karlley.jp@gmail.com>
2023-08-28 07:24:37 +0900

    Promiseでエラーハンドリング

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
+function main() {
+  fetchUserInfo("js-primer-example").catch((error) => {
+    // Promiseチェーン内のエラーを受け取る
+    console.error(`エラーが発生しました (${error}`);
+  });
+}
+
 function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+  // fetchの返り値のPromiseをreturn
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then((response) => {
-      console.log(response.status);
       if (!response.ok) {
-        console.error("エラーレスポンス", response); //ユーザーが取得できない時
+        //エラーレスポンスからRejectedなPromiseを作成して返す
+        return Promise.reject(
+          new Error(`${response.status}:${response.statusText}`)
+        );
       } else {
         return response.json().then((userInfo) => {
-          const view = escapeHTML`
-          <h4>${userInfo.name} (@${userInfo.login})</h4>
-          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-          <dl>
-            <dt>Location</dt>
-            <dt>${userInfo.location}</dt>
-            <dt>Repositories</dt>
-            <dd>${userInfo.public_repos}</dd>
-          </dl>
-          `;
-
-          const result = document.getElementById("result");
-          result.innerHTML = view;
+          //HTMLの組み立て
+          const view = createView(userInfo);
+          //HTMLの挿入
+          displayView(view);
         });
       }
     })
     .catch((error) => {
       console.error(error); //通信自体が失敗した時
     });
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+  <h4>${userInfo.name} (@${userInfo.login})</h4>
+  <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+  <dl>
+    <dt>Location</dt>
+    <dt>${userInfo.location}</dt>
+    <dt>Repositories</dt>
+    <dd>${userInfo.public_repos}</dd>
+  </dl>
+  `;
+}
+
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
 }
 
 function escapeSpecialCharas(str) {
